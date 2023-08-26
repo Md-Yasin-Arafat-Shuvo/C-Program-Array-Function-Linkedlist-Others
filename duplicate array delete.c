b476395aef461f68c1a3da5328849ea7e426d78e
Md Yasin Arafat Shuvo <83279549+Md-Yasin-Arafat-Shuvo@users.noreply.github.com>
2023-08-26 12:37:46 +0600

    Add files via upload

---
+++ duplicate array delete.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], freq[100];
+    int size, i, j, k, count;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;
+    }
+
+
+    for(i=0; i<size; i++)
+    {
+        count = 1;
+        for(j=i+1; j<size; j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+                freq[j] = 0;
+            }
+        }
+
+        if(freq[i] != 0)
+        {
+            freq[i] = count;
+        }
+    }
+
+    printf("\nFrequency of all elements of array : \n");
+    for(i=0; i<size; i++)
+    {
+        if(freq[i] != 0)
+        {
+            printf("%d occurs %d times\n", arr[i], freq[i]);
+        }
+
+    }
+    for(i=0; i<size; i++)
+    {
+        for(j=i+1; j<size; j++)
+        {
+
+            if(arr[i] == arr[j])
+            {
+
+                for(k=j; k<size; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                size--;
+                j--;
+            }
+        }
+    }
+    printf("\nArray elements after deleting duplicates : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+
+
+    return 0;
+}
